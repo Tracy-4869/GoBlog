935b62583e96b5ef1264acfbf679dff81c56c74d
changwei <1047796270@qq.com>
2022-02-19 23:16:01 +0800

    database

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	Id int
+}
+
 func init() {
+	mysqlDriver := beego.AppConfig.String("mysqldriver")
+	mysqlHost := beego.AppConfig.String("mysqlhost")
+	mysqlUser := beego.AppConfig.String("mysqluser")
+	mysqlPass := beego.AppConfig.String("mysqlpass")
+	mysqlPort := beego.AppConfig.String("mysqlport")
+	dataBase := beego.AppConfig.String("database")
+	charset := beego.AppConfig.String("charset")
+
+	datasource := mysqlUser + ":" + mysqlPass + "@tcp(" + mysqlHost + ":" + mysqlPort + ")/" + dataBase + "?charset=" + charset
+
+	orm.RegisterModel(new(User))
+
+	orm.RegisterDriver(mysqlDriver, orm.DRMySQL)
+	orm.RegisterDataBase("default", mysqlDriver, datasource)
+	orm.RunSyncdb("default", false, true)
 }
 
 func main() {
+	beego.Run()
 }
-
